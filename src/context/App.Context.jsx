36a6ebc26a643075b2b28ab94a6db3769ad6fd82
devserao <seraotourjo@gmail.com>
2025-11-17 21:04:48 +0600

    first commit

---
+++ src/context/App.Context.jsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(false);
+    const [showUserLogin, setShowUserLogin] = useState(false); // ✅ Add this
+
+    const value = {
+        navigate,
+        user,
+        setUser,             
+        isSeller,
+        setIsSeller,
+        showUserLogin,
+        setShowUserLogin     
+    };
+
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+export const useAppContext = () => useContext(AppContext);
